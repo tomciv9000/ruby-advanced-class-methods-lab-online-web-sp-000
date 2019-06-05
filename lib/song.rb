d6c435d978eaee5f684093c40a9f3087cd86f25b
Tom White <eugene.white@gmail.com>
2019-06-05 03:50:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,10 @@ class Song
     song
   end
   
-  def self.create_from_filename(mp3file)
+  def self.create_fsong = self.new 
     song = self.new
- 
+    song.name = mp3file.split(/[-.]/)[1].strip
+    song.artist_name = mp3file.split(/[-.]/)[0].strip
+    @@all << song
+  end
 end
